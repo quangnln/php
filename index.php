1b7cb207f157d705d201d60d53b3e9f4da727810
quangnln <79976854+quangnln@users.noreply.github.com>
2021-07-13 15:42:46 +0700

    123

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="utf-8">
+    <title>Học PHP</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+    <div id="main">
+        <div id="head" >
+            <H1>MOTPHANIT</H1>
+         </div>
+        <div id="menutop"> 
+            <ul>
+                <li> <a href="">CCNA</a></li>
+                <li> <a href="">MCSA</a></li>
+                <li> <a href="">SECURITY</a></li>
+            </ul>
+        </div>
+        <div id="maincontent">
+            <div id="content">
+                <div class="dinhdang">
+                    <h1>Nội Dung Tiêu Đề</h1>
+                    <div id="noidung">
+                        Cảm ơn vì ai đó đã viết nên những câu thơ hay về cuộc sống, để mọi người được đọc, được cảm nhận và được biết đến những vần thơ cuộc sống hay và ý nghĩa. Những bài thơ hay về cuộc sống tươi đẹp rất ý nghĩa và giàu cảm xúc, để ai đọc cũng cảm thấy xúc động, cảm thấy yêu cuộc sống của mình hơn.
+                    </div>
+                    
+                </div>
+
+            </div>
+        </div>
+        <div id="right">
+            <div class="dinhdang">
+                <h1>Tài liệu học tập</h1>
+                <div id="menuright">
+                    <ul>
+                        <li><a href="#">Tài Liệu ATTT</a></li>
+                        <li><a href="#">Tài Liệu CCNA</a></li>
+                        <li><a href="#">Tài Liệu MCSA</a></li>
+                        <li><a href="#">Tài Liệu ATTT</a></li>
+                    </ul>
+                    
+                </div>
+                
+            </div>
+        </div>
+        <div id="footer">
+            CopyRight: Nguyễn Long Nhật Quang. <br>
+        Phát triển website bởi Quang(IT)-coder tự do</div>
+    </div>
+</body>
+</html>
